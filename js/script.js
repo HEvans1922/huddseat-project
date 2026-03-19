a0800f0a552b00dda0998dc44e4cad4b54094491
HEvans1922 <harveyevans413@outlook.com>
2026-03-19 14:29:36 +0000

    Implement filter function for restaurant visibility
    Added filter function to manage restaurant visibility based on preferences.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
-const restuarants = {
-    
+const chaiwala = document.getElementById("chaiwala")
+const topTaste = document.getElementById("top-taste")
+const centros = document.getElementById("centros")
+
+function filter(pref) {
+    if (pref === "vegan") {
+        chaiwala.classList.add("visible")
+        topTaste.classList.add("visible")
+        centros.classList.remove("visible")
+    }
+
+    if (pref === "halal") {
+        chaiwala.classList.add("visible")
+        topTaste.classList.remove("visible")
+        centros.classList.remove("visible")
+    }
+
+    if (pref === "takeaway") {
+        chaiwala.classList.remove("visible")
+        topTaste.classList.add("visible")
+        centros.classList.add("visible")
+    }
+
+    if (pref === "eatIn") {
+        chaiwala.classList.add("visible")
+        topTaste.classList.remove("visible")
+        centros.classList.remove("visible")
+    }
+
+    if (pref === "clear") {
+        chaiwala.classList.add("visible")
+        topTaste.classList.add("visible")
+        centros.classList.add("visible")  
+    }
 }
+
